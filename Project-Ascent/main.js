cf271fd3ee0bdb39d90358502a01e6805a1e8695
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-06-27 11:30:28 -0400

    changed back to before

--- Project-Ascent/main.js
+++ Project-Ascent/main.js
@@ -4,8 +4,8 @@ import { galleryPage, formResult, battlePage, battleResults } from './app.js'
 // const appDiv = document.querySelector('#app');
 // app(appDiv);
 
-// const agentDiv = document.querySelector('#agents');
-// galleryPage(agentDiv)
+const agentDiv = document.querySelector('#agents');
+galleryPage(agentDiv)
 
 const team1 = document.querySelector('#form-results')
 formResult(team1)
@@ -17,15 +17,6 @@ const battleSec = document.querySelector('#battle-results');
 battleResults(battleSec)
 
 
-
-const main = () => {
-  const agentDiv = document.querySelector('#agents');
-  galleryPage(agentDiv);
-
-  // document.body.addEventListener('load', agentDiv)
-}
-
-main()
 
 
 
